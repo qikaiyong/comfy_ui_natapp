34bfa0afa9fff39e47fffd25c659999fb3a0583a
东华 <30590255+qikaiyong@users.noreply.github.com>
2025-07-27 18:56:32 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024-05-02 19:56
+# @Author  : 我的名字
+# @File    : __init__.py
+# @Description : 这个函数是用来balabalabala自己写
+import os
+
+print("### Load pinggy")
+
